22df1b8b482907edbb8b806bea9e069499955064
real-tom-hanks <lobstersurprises@gmail.com>
2020-07-19 20:11:07 -0700

    Updates

--- precache-manifest.5ff779050844767a32fe85943a5d8f4b.js
+++ precache-manifest.5ff779050844767a32fe85943a5d8f4b.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d1e669db4f8b9ea2de150d9755493b40",
+    "revision": "a702d95c3d84e307cf6b09515de0c51f",
     "url": "/index.html"
   },
   {
-    "revision": "c9704f975e78b5c607ef",
+    "revision": "be9f6938568827605284",
     "url": "/static/css/main.af754c7a.chunk.css"
   },
   {
-    "revision": "16db183b874e11b0fb05",
-    "url": "/static/js/2.865f5267.chunk.js"
+    "revision": "2c1bb5b2d00ea1d613ae",
+    "url": "/static/js/2.060c4724.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/static/js/2.865f5267.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.060c4724.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c9704f975e78b5c607ef",
-    "url": "/static/js/main.06051669.chunk.js"
+    "revision": "be9f6938568827605284",
+    "url": "/static/js/main.6bf8d3ed.chunk.js"
   },
   {
     "revision": "a30cbad6c0332059dfc4",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d20ab4140f8ba6c0a27722c26a8d9ed6.js"
+  "/precache-manifest.5ff779050844767a32fe85943a5d8f4b.js"
 );
 
 self.addEventListener('message', (event) => {
